b59c51b8cf099aa36556a09b14964c03d5b7cdc5
elderfieldzeus <elderfieldzeus24@gmail.com>
2024-03-20 09:30:59 +0800

    added mobile navbar

--- features/button_animation.js
+++ features/button_animation.js
@@ -1,9 +1,5 @@
 const button = document.getElementById("letsGetStarted");
 
-button.addEventListener("mouseover", () => {
-    button.className = "hovered";
-});
-
 button.addEventListener("mouseout", () => {
-    button.className = "released";
+    button.style.transitionDuration = "1s";
 });
---
+++ features/navigationBar.js
@@ -0,0 +1,45 @@
+const hook = document.querySelector(".hook");
+const navContainer = document.querySelector("nav");
+const navBar = [...document.querySelectorAll(".desktop>a")];
+const slideBar = document.querySelector(".slide-bar");
+const mobileButtons = document.querySelector(".navButtons");
+const wow = window.matchMedia("(min-width: 520px)");
+navBar.push(slideBar);
+
+navBar.forEach((nav) => {
+    nav.addEventListener("mouseout", () => {
+        nav.style.transitionDuration = "1s";
+    });
+});
+
+slideBar.addEventListener("click", () => {
+    if(navContainer.classList.contains("unactive")) {
+        navContainer.style.transitionDuration = "1s";
+        slideBar.classList.add("active");
+        slideBar.classList.remove("unactive");
+        setTimeout(() => {
+            mobileButtons.style.transitionDuration = "1s";
+            mobileButtons.style["display"] = "flex"
+        }, 800);
+        navContainer.classList.add("active");
+        navContainer.classList.remove("unactive");
+        hook.style.transitionDuration = "1s";
+        hook.style.filter = "brightness(75%)";
+    }
+    else {
+        navContainer.style.transitionDuration = "1s";
+        navContainer.classList.add("unactive");
+        navContainer.classList.remove("active");
+        mobileButtons.style["display"] = "none";
+        hook.style.filter = "brightness(100%)";
+    }
+});
+
+wow.addEventListener("change", () => {
+    if(wow.matches) {
+        navContainer.classList.remove("active");
+        navContainer.classList.add("unactive");
+        mobileButtons.style["display"] = "none";
+        mobileButtons.style["color"] = "green";
+    }
+});
